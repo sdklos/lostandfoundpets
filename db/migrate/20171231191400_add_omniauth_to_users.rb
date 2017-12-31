2ea7d4bdcfda1eaeb05da30ba9c6e74049e5a4d4
sukey <sdklos@gmail.com>
2017-12-31 14:57:15 -0500

    devise and omniauth facebook are now working

--- db/migrate/20171231191400_add_omniauth_to_users.rb
+++ db/migrate/20171231191400_add_omniauth_to_users.rb
@@ -4,7 +4,5 @@ class AddOmniauthToUsers < ActiveRecord::Migration[5.1]
     add_index :users, :provider
     add_column :users, :uid, :string
     add_index :users, :uid
-    add_column :users, :rake, :string
-    add_column :users, :db, :migrate
   end
 end
