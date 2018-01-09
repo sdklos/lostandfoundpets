3ecbef8320251df6bd7d31231523760c1d080286
sukey <sdklos@gmail.com>
2018-01-09 07:54:18 -0500

    set up basic browserrouter component

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={App} />
+  </Router>),
+  document.getElementById('root')
+);
 registerServiceWorker();
