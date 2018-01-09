51c2044b5e88efda3763519ddcf1d6b96065e0e4
sukey <sdklos@gmail.com>
2018-01-08 19:30:21 -0500

    models set up

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  has_many :pet_addresses
+  has_many :pets, through: :pet_addresses
+
+  geocoded_by :full_street_address
+  after_validation :geocode, :if => :full_street_address_changed?
+
+  def full_street_address
+    [street, city, state, zip_code].compact.join(', ')
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,7 @@
 class Pet < ApplicationRecord
+  has_many :pet_addresses
+  has_many :addresses, through: :pet_addresses
+
   has_attached_file :picture, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
