595078f9c15bf789a90a9c22817c621ed03e8bc6
sukey <sdklos@gmail.com>
2018-01-25 08:05:04 -0500

    began turning petform into stateless component

--- client/src/containers/NewPetContainer.js
+++ client/src/containers/NewPetContainer.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import PetForm from '../components/PetForm.js'
+import PetForm from '../components/PetForm.js';
+import { bindActionCreators } from 'redux';
+import { addPet, setFormState } from '../actions/index';
+import { connect } from 'react-redux';
+import SearchByLocation from '../components/SearchByLocation.js';
 
-export default class NewPetContainer extends Component {
+class NewPetContainer extends Component {
 
   render() {
   return (
-    <PetForm template="new" />
+    <PetForm
+      template="new"
+      formState={this.props.formState}
+       />
   )}
 }
+
+const mapStateToProps = state => {
+  return {
+    formState: state.formState,
+    breeds: state.breeds,
+    activePet: state.active
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    addPet: addPet,
+    setFormState: setFormState
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewPetContainer);
